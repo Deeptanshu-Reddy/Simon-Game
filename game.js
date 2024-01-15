09694b90fdb3f20f202d22fac13996730c99138e
Deeptanshu-Reddy <121843566+Deeptanshu-Reddy@users.noreply.github.com>
2024-01-15 17:59:20 -0600

    Update game.js

--- game.js
+++ game.js
@@ -42,7 +42,7 @@ function checkAnswer(currentLevel){
         setTimeout(function(){
             $("body").removeClass("game-over");
         },200);
-        $("h1").text("Game Over, Press Any Key to Restart");
+        $("h1").text("Game Over, you lost at Level"+ level +" Press Any Key to Restart");
         startOver();
     }
 }
